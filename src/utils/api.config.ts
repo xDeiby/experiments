964ecb86d82299ff2a81d345d2dcd86ee3258d53
Deiby Cabrera <55766812+xDeiby@users.noreply.github.com>
2021-09-19 16:30:30 -0300

    Update api.config.ts

--- src/utils/api.config.ts
+++ src/utils/api.config.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://experiment-api.herokuapp.com/api',
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://experiment-api.herokuapp.com/api',
 });
 
 export default api;
